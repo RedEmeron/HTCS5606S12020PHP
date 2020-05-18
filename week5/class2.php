2ee38943993733365ea00423a176aa18f1b49c03
David Kadlcik <david.kadlcik@gmail.com>
2020-05-19 09:37:32 +1200

    string operations

--- week5/class2.php
+++ week5/class2.php
@@ -13,3 +13,20 @@ $y = 5;
 echo "x =".$x;
 echo "y =".$y;
 echo "z = x + y = ".($x + $y);
+
+$greeting = "Hello world";
+//length of string
+echo "<br>".strlen($greeting)." string length<br>";
+//word count -- count number of words in that string
+echo str_word_count($greeting)." string word count<br>";
+//string reverse
+echo strrev($greeting)." string reverse<br>";
+//word string position
+echo strpos($greeting,"Hello")."word position<br>";
+//string replace
+echo str_replace($greeting, "Hello","byebye")." word replace<br>";
+//to uppercase
+echo strtoupper($greeting)."string to upper case<br>";
+//to lower
+echo strtolower($greeting)." To lower case<br>";
+

d6eeeb4dd6e2fbf12990d6c091f9deecb36e3058
David Kadlcik <david.kadlcik@gmail.com>
2020-05-19 10:00:59 +1200

    default time zone

--- week5/class2_2.php
+++ week5/class2_2.php
@@ -1,6 +1,8 @@
 <?php
+//set default time zone
+date_default_timezone_set('Pacific/Auckland');
 //take time from system or online
-//data function to take time out from system or online
+//data function to take time out from system or online https://www.php.net/manual/en/function.date.php
 $t = date("Y-m-d");
 $t1 = date("H:i:s");
 echo $t." ".$t1;

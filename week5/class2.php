6ed544a73597d8acc4c28306f6808937cef29966
David Kadlcik <david.kadlcik@gmail.com>
2020-05-19 09:06:02 +1200

    calculation

--- week5/class2.php
+++ week5/class2.php
@@ -6,4 +6,10 @@ $who = "david";
 echo $greeting;
 echo $who;
 
-echo "<p>".$greeting." ".$who."</p>";
+echo "<h1>".$greeting." ".$who."</h1>";
+
+$x = 4;
+$y = 5:
+echo "x =".$x;
+echo "y =".$y;
+echo "z = x + y = ".($x + $y);
